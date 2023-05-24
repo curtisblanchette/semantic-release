b53f2ee3c78373bd2eb9280afabc960fc0bb1402
curtisblanchette <hello@curtisblanchette.com>
2023-05-23 20:37:00 -0700

    fix: fix our punctuation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('Hello, World!');
 console.log('How are you?');
-console.log('Do some things');
+console.log('Do some things.');
 console.log('Another feature');
 console.log('This is the end.');
 console.log('Merge a PR on dev');
